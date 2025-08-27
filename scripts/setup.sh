811e4809061bf233b1a0f4e3c92f3522e58a5da0
isabelle248 <ilin28@crystal.org>
2025-08-26 22:05:41 -0700

    Create setup.sh

---
+++ scripts/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+echo "Installing system dependencies..."
+sudo apt-get update && sudo apt-get install -y openjdk-17-jdk python3 python3-pip unzip
+
+echo "Installing Python dependencies..."
+pip3 install --upgrade pip
+pip3 install -r requirements.txt
+
+echo "Installing Node dependencies..."
+npm install
+
+echo "Setup complete!"
